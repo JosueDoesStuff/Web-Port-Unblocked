9ddefe101c16d594ad7898d24d91efa51a63cf83
burnedpopcorn180 <164114576+burnedpopcorn@users.noreply.github.com>
2024-10-31 15:56:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://mathsspot.com';
+const nggUrl = 'https://www.autistici.org/burnedprojects/pt_noise_build/betterrunner.html';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'mathsspot.com') {
+    if (req.headers.host === 'autistici.org/burnedprojects/pt_noise_build/betterrunner.html') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
