63d266fc8a070abbda203922cb63554b62ccc227
JosueDoesStuff <104697070+JosueDoesStuff@users.noreply.github.com>
2024-12-22 18:04:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://play.geforcenow.com/mall/';
+const nggUrl = 'https://www.autistici.org/burnedprojects/undertale_web/';
 // just adding betterrunner.html doesn't work, so it has to be added into the Proxy's Future URL
 
 const proxy = createProxyMiddleware({
