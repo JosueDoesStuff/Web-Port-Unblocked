2480265ad2b60f0e4927fe10afa64085954191a2
JosueDoesStuff <104697070+JosueDoesStuff@users.noreply.github.com>
2024-12-22 19:08:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://nodiddydogwasatthediddyparty.global.ssl.fastly.net/';
+const nggUrl = 'https://www.easyfun.gg/cloud-games/brawl-stars.html';
 // just adding betterrunner.html doesn't work, so it has to be added into the Proxy's Future URL
 
 const proxy = createProxyMiddleware({
